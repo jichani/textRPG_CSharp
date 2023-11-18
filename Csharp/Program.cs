322e83a04987e667b8d2ae6dd2e5e88d29f286e4
jichani <jichan7571@naver.com>
2023-11-18 11:44:49 +0900

    초기 커밋.

---
+++ Csharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+// 객체 (OOP Object Oriented Programming)
+
+// Knight
+// 속성: hp, attak
+// 기능: Move, Attack, Die
+
+// Ref. 참조
+class Knight
+{
+    public int hp;
+    public int attack;
+
+    public void Move()
+    {
+        Console.WriteLine("Knight Move");
+    }
+
+    public void Attack() 
+    {
+        Console.WriteLine("Knight Attack");
+    }
+}
+// 복사
+struct Mage
+{
+    public int hp;
+    public int attack;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Knight knight = new Knight();
+
+        knight.hp = 100;
+        knight.attack = 10;
+
+        knight.Move();
+        knight.Attack();
+    }
+}
